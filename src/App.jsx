72ad6ca2508c8f7af7ed2fb982f7d04c541bd146
leeosilvp <leo965679@gmail.com>
2026-01-06 18:09:35 -0300

    build: instalando dependencias React-router-dom para navegação SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
