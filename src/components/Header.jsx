af2206ce7b6fa53eb5168f1637b43954a1e7dae4
leeosilvp <leo965679@gmail.com>
2026-01-06 19:43:26 -0300

    feat: criando função para exibir rota atual no Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useLocation } from 'react-router-dom'
 import logo from '../assets/svg/icon.svg'
 
+const routeNames = {
+    '/home': 'Home',
+    '/catalog': 'Catalog',
+    '/library': 'Library',
+    '/settings': 'Settings'
+}
+
 const Header = () => {
+
+    const location = useLocation()
+    const currentRoute = routeNames[location.pathname] || ''
+
     return (
         <header className="header-main">
             <section className='header-content'>
@@ -10,22 +21,22 @@ const Header = () => {
                         <img src={logo} alt="logo Snakr" />
                     </a>
                     <p>/</p>
-                    <Link>Home</Link>
+                    <Link>{currentRoute}</Link>
                 </article>
 
                 <article className='header-content-right'>
-                    <Link><i className='fa-regular fa-bell' /></Link>
-                    <Link><i className='fa-regular fa-bookmark' /></Link>
-                    <Link><i className='fa-regular fa-building' /></Link>
+                    <Link to='/notifications'><i className='fa-regular fa-bell' /></Link>
+                    <Link to='/wish-list'><i className='fa-regular fa-bookmark' /></Link>
+                    <Link to='https://github.com/leoosilvp'><i className='fa-regular fa-building' /></Link>
                 </article>
             </section>
 
             <nav>
                 <ul>
-                    <NavLink to='/home' className={({isActive}) => isActive ? 'active' : ''}>Home</NavLink>
-                    <NavLink to='/catalog' className={({isActive}) => isActive ? 'active' : ''}>Catalog</NavLink>
-                    <NavLink to='/library' className={({isActive}) => isActive ? 'active' : ''}>Library</NavLink>
-                    <NavLink to='/settings' className={({isActive}) => isActive ? 'active' : ''}>Settings</NavLink>
+                    <NavLink to='/home' className={({ isActive }) => isActive ? 'active' : ''}>Home</NavLink>
+                    <NavLink to='/catalog' className={({ isActive }) => isActive ? 'active' : ''}>Catalog</NavLink>
+                    <NavLink to='/library' className={({ isActive }) => isActive ? 'active' : ''}>Library</NavLink>
+                    <NavLink to='/settings' className={({ isActive }) => isActive ? 'active' : ''}>Settings</NavLink>
                 </ul>
             </nav>
         </header>
