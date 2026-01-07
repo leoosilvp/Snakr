44204898bcd5e5721ad0f99b97143480aa45a655
leeosilvp <leo965679@gmail.com>
2026-01-06 23:17:35 -0300

    feat: integrando Ad com api

--- src/components/Ads.jsx
+++ src/components/Ads.jsx
@@ -1,16 +1,39 @@
+import { useAds } from '../hooks/useAds'
 import { Link } from "react-router-dom"
 import '../css/ads.css'
 
 const Ads = () => {
+
+    const { ads, loading, error } = useAds()
+
+    if (loading) {
+        return <div className='ad-loading' />
+    }
+
+    if (error) {
+        return <div className='ad-erro'>Erro: {error}</div>
+    }
+
+    if (!ads.length) {
+        return <div className='ad-erro'>Nenhum anúncio disponível</div>
+    }
+
     return (
-        <article className="ads">
-            <video src="https://anonymousai-hub.github.io/DB/video/upd-1.5.mp4" controls />
-            <div className="ads-content">
-                <h1>Bem-vindo Graham 1.5</h1>
-                <p>Sua mais nova plataforma de inteligência artificial!</p>
-                <Link>Saiba mais</Link>
-            </div>
-        </article>
+        ads.map(ad => (
+            <article className="ads">
+                {ad.videoUrl && (
+                    <video src={ad.videoUrl} controls preload='metadata' />
+                )}
+                {ad.imgUrl && (
+                    <img src={ad.imgUrl} />
+                )}
+                <div className="ads-content">
+                    <h1>{ad.title}</h1>
+                    <p>{ad.description}</p>
+                    <Link>Saiba mais</Link>
+                </div>
+            </article>
+        ))
     )
 }
 
