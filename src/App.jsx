7452d2bb28d2f8212053c8ee499ae6c591415b1c
leeosilvp <leo965679@gmail.com>
2026-01-08 17:42:04 -0300

    feat: criando pagina de erro 'Page not found'

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Home from "./routes/Home"
 import ChangeIcon from './hooks/ChangeIcon'
+import Error from "./routes/Error";
 
 function App() {
-  
+
   ChangeIcon();
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to='/home'/>}/>
-        <Route path="/home" element={<Home/>}/>
+        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Navigate to='/home' />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/routes/Error.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+import img from '../assets/svg/error.svg'
+import logo from '../assets/svg/logo2.svg'
+
+const Error = () => {
+  return (
+    <main className="error-page">
+      <header>
+        <a href="/">
+          <img src={logo} alt="logo-snakr" />
+        </a>
+
+        <Link to='/docs'><i className='fa-solid fa-book-open'></i></Link>
+      </header>
+
+      <section className='error-page-content'>
+        <h1>Oops!...</h1>
+        <img src={img} />
+        <h1>Page not found</h1>
+        <p>You weren't supposed to be here — it happens.<br />If you need help, contact us.</p>
+      </section>
+
+      <section className='error-page-btns'>
+        <Link to='/'>Home</Link>
+        <Link to='/docs'>Contact us</Link>
+      </section>
+    </main>
+  )
+}
+
+export default Error
