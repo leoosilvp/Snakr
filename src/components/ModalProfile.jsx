44112b7981cca4fe7adee2a5c93b5fa8c837ba8c
leeosilvp <leo965679@gmail.com>
2026-01-14 19:24:01 -0300

    feat: adicionando icons nos Links do modalProfile

--- src/components/ModalProfile.jsx
+++ src/components/ModalProfile.jsx
@@ -2,42 +2,42 @@ import { forwardRef } from 'react'
 import { Link } from 'react-router-dom'
 
 const ModalProfile = forwardRef(
-  ({ isOpen, onMouseEnter, onMouseLeave }, ref) => {
-    if (!isOpen) return null
+    ({ isOpen, onMouseEnter, onMouseLeave }, ref) => {
+        if (!isOpen) return null
 
-    return (
-      <article
-        ref={ref}
-        className="modal-profile"
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      >
-        <section className="modal-profile-info">
-          <img src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg" />
-          <div>
-            <h1>user5679</h1>
-            <h2>usuario@gmail.com</h2>
-          </div>
-        </section>
+        return (
+            <article
+                ref={ref}
+                className="modal-profile"
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+            >
+                <section className="modal-profile-info">
+                    <img src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg" />
+                    <div>
+                        <h1>user5679</h1>
+                        <h2>usuario@gmail.com</h2>
+                    </div>
+                </section>
 
-        <section className="modal-profile-btns">
-          <Link to="/profile">Profile</Link>
-          <Link to="/home">Home</Link>
-          <Link to="/catalog">Catalog</Link>
-          <Link to="/library">Library</Link>
-          <Link to="/wish-list">Wish list</Link>
-          <hr />
-          <Link to="/settings">Settings</Link>
-          <Link to="/settings/support">Support</Link>
-          <Link to="/docs">Docs</Link>
-          <hr />
-          <Link to="/settings/apparence">Apparence</Link>
-          <hr />
-          <Link className="active">Sign Out</Link>
-        </section>
-      </article>
-    )
-  }
+                <section className="modal-profile-btns">
+                    <Link to='/profile'><i className="fa-regular fa-user" />Profile</Link>
+                    <Link to='/home'><i className="fa-regular fa-home" />Home</Link>
+                    <Link to='/catalog'><i className="fa-solid fa-list" />Catalog</Link>
+                    <Link to='/library'><i className="fa-solid fa-grip" />Library</Link>
+                    <Link to='/wish-list'><i className="fa-regular fa-bookmark" />Wish list</Link>
+                    <hr />
+                    <Link to='/settings'><i className="fa-solid fa-gear" />Settings</Link>
+                    <Link to='/settings/support'><i className="fa-regular fa-life-ring" />Support</Link>
+                    <Link to='/docs'><i className="fa-solid fa-book-open" />Docs</Link>
+                    <hr />
+                    <Link to='/settings/apparence'><i className="fa-solid fa-paintbrush" />Apparence</Link>
+                    <hr />
+                    <Link className="active"><i className="fa-solid fa-arrow-right-from-bracket" />Sign Out</Link>
+                </section>
+            </article>
+        )
+    }
 )
 
 export default ModalProfile
