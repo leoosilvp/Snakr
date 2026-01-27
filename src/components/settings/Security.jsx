62e226082a9651c0d0f2ffbae0fe51e5017a0af7
leeosilvp <leo965679@gmail.com>
2026-01-27 02:21:45 -0300

    feat: criando função log-out em Security de Settings

--- src/components/settings/Security.jsx
+++ src/components/settings/Security.jsx
@@ -1,8 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../../services/auth'
 import { useUser } from '../../hooks/useUser'
 
 const Security = () => {
 
+  const navigate = useNavigate()
   const { user } = useUser();
+
+  const handleLogout = async (e) => {
+    e.preventDefault()
+
+    try {
+      await logout()
+      navigate('/login', { replace: true })
+      window.location.reload()
+    } catch (err) {
+      console.error('Logout failed', err)
+    }
+  }
 
   return (
     <section className="settings-security">
@@ -14,13 +29,13 @@ const Security = () => {
 
       <h1>Password</h1>
       <input className='readOnly' type="password" value={'**************'} readOnly />
-      <button className='reset-password' onClick={() => window.location.href='/security/reset-password'}>Reset password</button>
+      <button className='reset-password' onClick={() => window.location.href = '/security/reset-password'}>Reset password</button>
 
       <hr />
 
       <section className='settings-security-session'>
         <h2>Log out of this device</h2>
-        <button>Sign out</button>
+        <button onClick={handleLogout}>Sign out</button>
       </section>
 
       <hr />
@@ -28,7 +43,7 @@ const Security = () => {
       <section className='settings-security-all-session'>
         <div className='settings-security-session'>
           <h2>Sign out on all devices</h2>
-          <button className='active'>Log out for everyone</button>
+          <button className='active' onClick={handleLogout}>Log out for everyone</button>
         </div>
         <p>Sign out of all active sessions on all devices, including your current session. It may take up to 15 minutes for other devices to sign out.</p>
       </section>
