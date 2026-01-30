70797805b886ef2e8c9b668eaadd2f2ba020e833
leeosilvp <leo965679@gmail.com>
2026-01-29 22:30:03 -0300

    feat: transformando Collection de Profile em componente externo

---
+++ src/components/profile/Collection.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom"
+
+const Collection = () => {
+  return (
+    <section className='profile-collection'>
+      <header className='profile-collection-header'>
+        <h1>My collection</h1>
+        <Link><i className='fa-solid fa-pencil' /> Edit collection</Link>
+      </header>
+      <section className='profile-collection-content'>
+        <section className='profile-statistics-in-collection'>
+          <div>
+            <h1>0</h1>
+            <Link>Games on account</Link>
+          </div>
+          <div>
+            <h1>0</h1>
+            <Link>Analytics</Link>
+          </div>
+          <div>
+            <h1>0</h1>
+            <Link>Total hours played</Link>
+          </div>
+          <div>
+            <h1>0</h1>
+            <Link>On the wish list</Link>
+          </div>
+        </section>
+        <section className='profile-ctn-featured-games'>
+          <h1>Featured games</h1>
+          <section className='profile-featured-games'>
+          </section>
+        </section>
+      </section>
+    </section>
+  )
+}
+
+export default Collection
--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -5,6 +5,8 @@ import background from '../assets/img/background-default.png'
 import { useUser } from '../hooks/useUser'
 import { Link } from 'react-router-dom'
 
+import Collection from '../components/profile/Collection'
+
 
 const Profile = () => {
 
@@ -29,37 +31,7 @@ const Profile = () => {
             </section>
           </header>
 
-          <section className='profile-collection'>
-            <header className='profile-collection-header'>
-              <h1>My collection</h1>
-              <Link><i className='fa-solid fa-pencil' /> Edit collection</Link>
-            </header>
-            <section className='profile-collection-content'>
-              <section className='profile-statistics-in-collection'>
-                <div>
-                  <h1>0</h1>
-                  <Link>Games on account</Link>
-                </div>
-                <div>
-                  <h1>0</h1>
-                  <Link>Analytics</Link>
-                </div>
-                <div>
-                  <h1>0</h1>
-                  <Link>Total hours played</Link>
-                </div>
-                <div>
-                  <h1>0</h1>
-                  <Link>On the wish list</Link>
-                </div>
-              </section>
-              <section className='profile-ctn-featured-games'>
-                <h1>Featured games</h1>
-                <section className='profile-featured-games'>
-                </section>
-              </section>
-            </section>
-          </section>
+          <Collection />
 
         </section>
         <aside className='profile-aside'>
