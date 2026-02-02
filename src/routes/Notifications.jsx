5db7fca49786b82ea36ed7478f693d5343e7407b
leeosilvp <leo965679@gmail.com>
2026-02-01 23:44:40 -0300

    feat: criando Header da page Notifications

--- src/routes/Notifications.jsx
+++ src/routes/Notifications.jsx
@@ -9,7 +9,16 @@ const Notifications = () => {
         <main className='notifications-main'>
             <Header />
             <section className='notifications-content'>
-                
+                <header className='notifications-content-header'>
+                    <section className='notifications-content-header-left'>
+                        <button className='active'>All</button>
+                        <button>Unread <span>0</span></button>
+                    </section>
+                    <section className='notifications-content-header-right'>
+                        <button>Mark all as read</button>
+                        <button>Clear all</button>
+                    </section>
+                </header>
             </section>
             <Footer />
         </main>
