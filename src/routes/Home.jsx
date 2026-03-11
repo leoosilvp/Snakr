86cda96d513a11b72a659f55ec314d86bb0f991a
leeosilvp <leo965679@gmail.com>
2026-03-10 23:50:23 -0300

    feat: criando estrutura minima para card-game de Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -9,14 +9,29 @@ const Home = () => {
 
       <section className="content">
         <div className="home-main-feed">
+
           <section className="most-popular">
             <header className="most-popular-header">
               <h1>Most popular</h1>
               <div />
             </header>
+            <div className="most-popular-grid">
+              <article className="card-game-main">
+                <img src="https://images.igdb.com/igdb/image/upload/t_cover_big/co670h.jpg" />
+                <section className="card-game-main-content">
+                  <h1>Titulo do jogo</h1>
+                  <div>
+                    <p>ação</p>
+                    <p>aventura</p>
+                    <p>corrida</p>
+                  </div>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis eaque impedit omnis provident qui temporibus itaque, sapiente iusto </p>
+                </section>
+              </article>
+            </div>
           </section>
-        </div>
 
+        </div>
       </section>
 
       <Footer />
