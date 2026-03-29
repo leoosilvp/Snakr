37b4d1698001de1a23d853e7973bfa7f350e0128
leeosilvp <leo965679@gmail.com>
2026-03-29 18:54:35 -0300

    feat: criando area para selecionar os jogos em ModalEditCollection em Collection de Profile

--- src/components/profile/ModalEditCollection.jsx
+++ src/components/profile/ModalEditCollection.jsx
@@ -1,3 +1,4 @@
+import { Plus } from "@geist-ui/icons"
 
 const ModalEditCollection = () => {
     return (
@@ -10,6 +11,35 @@ const ModalEditCollection = () => {
                         <button className="active">Save</button>
                     </div>
                 </header>
+                <section className="modal-edit-collection-content">
+                    <h1>Some games fade away… and some stay with you. Keep the ones that remain with you here.</h1>
+                    <p>You can highlight up to 6 games. To add a new one, remove one of the current ones.</p>
+                    <div className="modal-edit-collection-grid">
+                        <article className="modal-edit-collection-card">
+                            <Plus />
+                        </article>
+
+                        <article className="modal-edit-collection-card">
+                            <Plus />
+                        </article>
+
+                        <article className="modal-edit-collection-card">
+                            <Plus />
+                        </article>
+
+                        <article className="modal-edit-collection-card">
+                            <Plus />
+                        </article>
+
+                        <article className="modal-edit-collection-card">
+                            <Plus />
+                        </article>
+
+                        <article className="modal-edit-collection-card">
+                            <Plus />
+                        </article>
+                    </div>
+                </section>
             </article>
         </main>
     )
