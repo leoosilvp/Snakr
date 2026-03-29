afb8c614acfc033f39c21a4be92233ce389ffcba
leeosilvp <leo965679@gmail.com>
2026-03-28 21:04:47 -0300

    feat: criando header do ModalEditCollection

--- src/components/profile/ModalEditCollection.jsx
+++ src/components/profile/ModalEditCollection.jsx
@@ -1,9 +1,17 @@
 
 const ModalEditCollection = () => {
     return (
-        <article className="modal-edit-collection">
-
-        </article>
+        <main className="modal-edit-collection-main">
+            <article className="modal-edit-collection">
+                <header className="modal-edit-collection-header">
+                    <h1>Edit Collection</h1>
+                    <div>
+                        <button>Cancel</button>
+                        <button className="active">Save</button>
+                    </div>
+                </header>
+            </article>
+        </main>
     )
 }
 
