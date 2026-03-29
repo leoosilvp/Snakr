f9400b57fa9c392955ff856190d61b26aceae8bd
leeosilvp <leo965679@gmail.com>
2026-03-29 19:53:18 -0300

    style: criando estilização de SearchForCollection

--- src/components/profile/SearchForCollection.jsx
+++ src/components/profile/SearchForCollection.jsx
@@ -1,4 +1,4 @@
-import { Search } from "@geist-ui/icons"
+import { Award, Search } from "@geist-ui/icons"
 
 const SearchForCollection = () => {
     return (
@@ -6,12 +6,28 @@ const SearchForCollection = () => {
             <section className="search-for-collection-content">
                 <header className="search-for-collection-header">
                     <Search size={16} />
-                    <input type="text" placeholder="search.." />
+                    <input type="text" placeholder="search.." maxLength={55} />
                 </header>
                 <section className="search-for-collection-grid">
                     <article className="search-for-collection-card">
-
+                        <img src="https://images.igdb.com/igdb/image/upload/t_cover_big/co1wz4.jpg" />
+                        <div className="search-for-collection-card-content">
+                            <h1>The Witcher 3: Wild Hunt - Game of the Year Edition</h1>
+                            <article className="game-achievements">
+                                <section>
+                                    <div>
+                                        <Award size={16} />
+                                        <p>0</p>
+                                    </div>
+                                    <div>
+                                        <p>0%</p>
+                                    </div>
+                                </section>
+                                <div className="progress-bar" />
+                            </article>
+                        </div>
                     </article>
+                    <hr />
                 </section>
             </section>
         </main>
