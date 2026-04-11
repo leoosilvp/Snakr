44d145c27ee05d643e2249a0b6c803a21140d422
leeosilvp <leo965679@gmail.com>
2026-04-10 22:41:23 -0300

    feat: criando aviso de nenhum download em andamento em Downloads

--- src/routes/Downloads.jsx
+++ src/routes/Downloads.jsx
@@ -1,6 +1,7 @@
 import '../css/download.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { Download } from '@geist-ui/icons'
 
 const Downloads = () => {
     return (
@@ -12,6 +13,13 @@ const Downloads = () => {
                     <h1>0</h1>
                     <hr />
                 </header>
+
+                <section className='download-content-grid'>
+                    <div className='no-downloads'>
+                        <Download size={25} />
+                        <p>No downloads in progress.</p>
+                    </div>
+                </section>
             </section>
             <Footer />
         </main>
